1001e7e25be584bb59f74faac878d123ccc37048
tobi5775 <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>
2021-11-25 14:38:23 +0100

    change ids to string

--- src/main/java/de/apnmt/payment/common/repository/PriceRepository.java
+++ src/main/java/de/apnmt/payment/common/repository/PriceRepository.java
@@ -1,12 +1,19 @@
 package de.apnmt.payment.common.repository;
 
 import de.apnmt.payment.common.domain.Price;
+import de.apnmt.payment.common.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Spring Data SQL repository for the Price entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface PriceRepository extends JpaRepository<Price, Long> {}
+public interface PriceRepository extends JpaRepository<Price, String> {
+
+    List<Price> findAllByProduct(Product product);
+
+}
--- src/main/java/de/apnmt/payment/common/repository/SubscriptionRepository.java
+++ src/main/java/de/apnmt/payment/common/repository/SubscriptionRepository.java
@@ -1,12 +1,19 @@
 package de.apnmt.payment.common.repository;
 
+import de.apnmt.payment.common.domain.Customer;
 import de.apnmt.payment.common.domain.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Spring Data SQL repository for the Subscription entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {}
+public interface SubscriptionRepository extends JpaRepository<Subscription, String> {
+
+    List<Subscription> findAllByCustomer(Customer customer);
+
+}
