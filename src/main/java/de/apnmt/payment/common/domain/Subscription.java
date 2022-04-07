9da22a0c0b2516413a858d52c2fbe234d0b399cf
tobi5775 <tobias.wittmann96@web.de>
2022-04-07 19:24:00 +0200

    fetch subscription items eager

--- src/main/java/de/apnmt/payment/common/domain/Subscription.java
+++ src/main/java/de/apnmt/payment/common/domain/Subscription.java
@@ -5,13 +5,7 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -31,7 +25,7 @@ public class Subscription implements Serializable {
     @Column(name = "expiration_date", nullable = false)
     private LocalDateTime expirationDate;
 
-    @OneToMany(mappedBy = "subscription", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "subscription", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JsonIgnoreProperties(value = {"price", "subscription"}, allowSetters = true)
     private Set<SubscriptionItem> subscriptionItems = new HashSet<>();
 
